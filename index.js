368f0f164ef74e820f665b975055e21559f0fb5f
Andrey Botero <andrey.botero2018@gmail.com>
2024-05-08 14:24:12 -0300

    feat: Add error handling to hero endpoints

--- index.js
+++ index.js
@@ -18,14 +18,22 @@ app.use(express.json());
 //Get all heróis
 app.get("/herois", async (req, res) => {
   const { rows } = await pool.query("SELECT * FROM herois");
-  res.json(rows);
+  try {
+    res.json(rows);
+  } catch (err) {
+    console.error("Não foi possível retornar os heróis!");
+  }
 });
 
-//Get all heróis by id
+//Get heróis by id
 app.get("/herois/:id", async (req, res) => {
   const { id } = req.params;
   const { rows } = await pool.query("SELECT * FROM herois WHERE id = $1", [id]);
-  res.json(rows);
+  try {
+    res.json(rows);
+  } catch (err) {
+    console.error("Não foi possível retornar o herói!");
+  }
 });
 
 //Create herói
@@ -35,7 +43,16 @@ app.post("/herois", async (req, res) => {
     "INSERT INTO herois (nome_herois, poder, hp, nivel) VALUES ($1, $2, $3, $4)",
     [nome_herois, poder, hp, nivel]
   );
-  res.json({ message: "Herói criado com sucesso!" });
+  try {
+    res.json({
+      message: "Herói criado com sucesso!",
+      body: {
+        heroi: { nome_herois, poder, hp, nivel },
+      },
+    });
+  } catch (err) {
+    console.error("Não foi possível criar o herói!");
+  }
 });
 
 //Update herói
@@ -46,14 +63,29 @@ app.put("/herois/:id", async (req, res) => {
     "UPDATE herois SET nome_herois = $1, poder = $2, hp = $3, nivel = $4 WHERE id = $5",
     [nome_herois, poder, hp, nivel, id]
   );
-  res.json({ message: "Herói atualizado com sucesso!" });
+  try {
+    res.json({
+      message: "Herói atualizado com sucesso!",
+      body: {
+        heroi: { nome_herois, poder, hp, nivel },
+      },
+    });
+  } catch (err) {
+    console.error("Não foi possível atualizar o herói!");
+  }
 });
 
 //Delete herói
 app.delete("/herois/:id", async (req, res) => {
   const { id } = req.params;
   await pool.query("DELETE FROM herois WHERE id = $1", [id]);
-  res.json({ message: "Herói deletado com sucesso!" });
+try {
+    res.json({
+      message: "Herói deletado com sucesso!",
+    });
+} catch (err) {
+    console.error("Não foi possível deletar o herói!");
+  }
 });
 
 app.listen(PORT, () => {
